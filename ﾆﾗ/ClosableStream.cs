cbf04c89da087ffef71581a0a0839ce43392094b
sasaki <info@ftp.gawanative.com>
2019-04-23 14:59:55 +0900

    Spool

---
+++ ﾆﾗ/ClosableStream.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ﾆﾗ
+{
+    public class ClosableStream : Stream
+    {
+        UltraSuperSpool spool;
+        Stream stream;
+        internal ClosableStream(UltraSuperSpool spool, Stream stream)
+        {
+            this.spool = spool;
+            this.stream = stream;
+        }
+
+        public override bool CanRead => stream.CanRead;
+
+        public override bool CanSeek => stream.CanSeek;
+
+        public override bool CanWrite => stream.CanWrite;
+
+        public override long Length => stream.Length;
+
+        public override long Position { get => stream.Position; set => stream.Position = value; }
+
+        public override void Flush() => stream.Flush();
+
+
+        public override int Read(byte[] buffer, int offset, int count) => stream.Read(buffer, offset, count);
+
+        public override long Seek(long offset, SeekOrigin origin) => stream.Seek(offset, origin);
+
+
+        public override void SetLength(long value) => stream.SetLength(value);
+
+        public override void Write(byte[] buffer, int offset, int count) => stream.Write(buffer, offset, count);
+    }
+}
---
+++ ﾆﾗ/UltraSuperSpool.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ﾆﾗ
+{
+    public class UltraSuperSpool
+    {
+        protected HashSet<Task<bool>> Tasks {
+            [MethodImpl(MethodImplOptions.Synchronized)]
+            get;
+            [MethodImpl(MethodImplOptions.Synchronized)]
+            private set;
+        } = new HashSet<Task<bool>>();
+
+        public int TaskCount {
+            [MethodImpl(MethodImplOptions.Synchronized)]
+            get;
+            [MethodImpl(MethodImplOptions.Synchronized)]
+            private set;
+        } = 0;
+
+        public ClosableStream CreateClosableStream(Stream stream)
+        {
+            return new ClosableStream(this, stream);
+        }
+    }
+}
